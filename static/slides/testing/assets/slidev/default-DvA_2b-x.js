002d8c2c122f12fefba8009f60768be0ae48f4e3
Catherine Pope <me@catherinepope.com>
2025-10-07 09:24:24 +0100

    Removed slidev experiment

--- static/slides/testing/assets/slidev/default-DvA_2b-x.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-CPAV6JZe.js";import{_ as r}from"../index-Dc4m9TJx.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
