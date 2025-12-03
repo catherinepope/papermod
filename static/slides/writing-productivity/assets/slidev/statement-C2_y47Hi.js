f1301a04547f42ff47b23f60a813b82456ec0770
Catherine Pope <me@catherinepope.com>
2025-12-03 09:35:24 +0000

    Added writing productivity slides

---
+++ static/slides/writing-productivity/assets/slidev/statement-C2_y47Hi.js
@@ -0,0 +1,1 @@
+import{f as t,o as s,g as o,j as a}from"../modules/vue-6oM9PBC_.js";import{u as r}from"./context-f7TIcqRp.js";const n={class:"slidev-layout statement"},c={class:"my-auto"},i={__name:"statement",setup(_){return r(),(e,l)=>(s(),t("div",n,[o("div",c,[a(e.$slots,"default")])]))}};export{i as _};
