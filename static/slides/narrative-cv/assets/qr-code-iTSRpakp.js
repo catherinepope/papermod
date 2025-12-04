baed8d78a0ae2f42482cb3b3dcec855ad63bb83c
Catherine Pope <me@catherinepope.com>
2025-12-04 09:22:12 +0000

    Added narrative CV slides

---
+++ static/slides/narrative-cv/assets/qr-code-iTSRpakp.js
@@ -0,0 +1,1 @@
+const s="/slides/narrative-cv/images/qr-code.png";export{s as _};
---
+++ static/slides/narrative-cv/assets/slidev/default-DpZ6WIY-.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-eV9a54mL.js";import{_ as r}from"../index-Dh7P9Lo2.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
