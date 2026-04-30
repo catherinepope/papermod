48f4958956028789231558cf25ed7531d76157d1
Catherine Pope <me@catherinepope.com>
2026-04-30 12:48:42 +0100

    Added ABM slides

---
+++ static/slides/abm/assets/slidev/default-CEveRdmf.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-B_YCiXHb.js";import{_ as r}from"../index-TcdxlpKC.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
